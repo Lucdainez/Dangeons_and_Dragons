1836ed2d9543bdab984e5aa8a11b524751a6b2da
lucdainez <lucdainez@gmail.com>
2023-01-18 17:30:59 -0300

    feat(classe Halfling): classe estendida da classe Race criada

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+export default class Halfling extends Race {
+  private _maxLife: number;
+  static _classInstances = 0; 
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLife = 60;
+    Halfling._classInstances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLife;
+  }
+
+  static createdRacesInstances(): number {
+    return Halfling._classInstances;
+  }
+}
