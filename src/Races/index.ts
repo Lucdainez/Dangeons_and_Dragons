48b705244c28f30d31175403eacba08c3030b921
lucdainez <lucdainez@gmail.com>
2023-01-18 17:31:38 -0300

    feat(index.ts classe Race): adicionar importações

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,3 +1,9 @@
+import Dwarf from './Dwarf';
+import Elf from './Elf';
+import Halfling from './Halfling';
+import Orc from './Orc';
 import Race from './Race';
 
 export default Race;
+
+export { Dwarf, Elf, Halfling, Orc };
