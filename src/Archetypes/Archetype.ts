71d7c84397f1aaee0a905c55a1ea394d01f84925
lucdainez <lucdainez@gmail.com>
2023-01-18 17:57:20 -0300

    feat(classe abstrata Archetype): super classe criada

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,25 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+
+  constructor(name: string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  get name(): string { return this._name; } 
+
+  get special(): number { return this._special; } 
+
+  get cost(): number { return this._cost; } 
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
