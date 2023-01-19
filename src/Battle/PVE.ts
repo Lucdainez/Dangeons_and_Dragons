6fc7fed25b67e2de84ebf5eb2caf78ed05141e27
lucdainez <lucdainez@gmail.com>
2023-01-19 13:33:04 -0300

    feat(classe PVE): classe criada e implementada

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,29 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+export default class PVE extends Battle {
+  private _player1: Fighter;
+  private _monsters: (Fighter | SimpleFighter)[];
+
+  constructor(
+    player: Fighter,
+    monsters: (Fighter | SimpleFighter)[],
+  ) {
+    super(player);
+    this._player1 = player;
+    this._monsters = monsters;
+  }
+
+  public fight(): number {
+    const player1 = this._player1;
+    const monsters = this._monsters;
+    monsters.forEach((monster) => {
+      if (player1.lifePoints > 0 && monsters
+        .some((beast) => beast.lifePoints > 0)) {
+        monster.receiveDamage(player1.strength);
+        player1.receiveDamage(monster.strength);
+      }
+    });
+    return super.fight();
+  }
+}
